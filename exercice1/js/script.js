c7390c530be5d72332d55d22297340370f841b82
felikarina <felikarina@yahoo.fr>
2023-10-12 12:22:28 +0200

    première version

--- exercice1/js/script.js
+++ exercice1/js/script.js
@@ -1,1 +1,9 @@
+let PrenomUtilisatrice = prompt("Quel est votre prénom ?")
+let bienvenue = ("👋 Bonjour " + PrenomUtilisatrice)
 
+document.body.innerHTML += bienvenue
+
+let AnneedeNaissance = parseInt(prompt("Quelle est votre année de naissance ?"))
+let age = 2023 - AnneedeNaissance
+
+document.body.innerHTML += age
